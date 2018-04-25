fd3ded6990f849a8f13f5919de529d5df83b7f99
tkoar <terrancekoar@gmail.com>
2018-04-25 15:53:06 -0400

    added tests

--- test/index_test.py
+++ test/index_test.py
@@ -1,18 +1,32 @@
 import unittest2 as unittest
 from ipynb.fs.full.index import *
 
-class TestInstanceMethods(unittest.TestCase):
+class TestObjectAttributes(unittest.TestCase):
 
-    def test_instance_types(self):
-        self.assertEqual(type(meryl), type(Driver()))
-        self.assertEqual(type(daniel), type(Driver()))
-        self.assertEqual(type(niky), type(Passenger()))
-        self.assertEqual(type(terrance), type(Passenger()))
+    def test_driver_instance_methods_decorators(self):
+        new_driver = Driver()
+        new_driver.first = "Ron"
+        new_driver.last = "Burgundy"
+        new_driver.miles_driven = 20
+        new_driver.rating = 3.0
+        self.assertEqual(new_driver._first, "Ron")
+        self.assertEqual(new_driver.first, "Ron")
+        self.assertEqual(new_driver._last, "Burgundy")
+        self.assertEqual(new_driver.last, "Burgundy")
+        self.assertEqual(new_driver._miles_driven, "20")
+        self.assertEqual(new_driver.miles_driven, "20")
+        self.assertEqual(new_driver._rating, "3.0")
+        self.assertEqual(new_driver.rating, "3.0")
 
-    def test_variables(self):
-        self.assertEqual(polite_greeting, "Hey, how are you?")
-        self.assertEqual(no_time_to_talk, "Punch it! They're on our tail!")
-
-    def test_instance_methods(self):
-        self.assertEqual(daniel.greeting(), "Hey, how are you?")
-        self.assertEqual(niky.in_a_hurry(), "Punch it! They're on our tail!")
+    def test_passenger_instance_methods_decorators(self):
+        new_passenger = Passenger()
+        new_passenger.first = "Melissa"
+        new_passenger.last = "Morris"
+        new_passenger.email = "melissa.morris@gmail.com"
+        self.assertEqual(new_passenger._first, "Melissa")
+        self.assertEqual(new_passenger.first, "Melissa")
+        self.assertEqual(new_passenger._last, "Morris")
+        self.assertEqual(new_passenger.last, "Morris")
+        self.assertEqual(new_passenger._email, "melissa.morris@gmail.com")
+        self.assertEqual(new_passenger.email, "melissa.morris@gmail.com")
+        self.assertEqual(new_passenger.yell_name(), "MELISSA MORRIS")
